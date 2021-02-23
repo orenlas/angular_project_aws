7656c214c67c27322111ddcee16a44a7337c8a04
Ishay Siboni <yshaisiboni@gmail.com>
2021-02-23 13:27:59 +0200

    firebase connect and angular material

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyACpRha_w7SpbUU1_zmvwiaBMV16Cu7cMI",
+    authDomain: "finalproject-you.firebaseapp.com",
+    projectId: "finalproject-you",
+    storageBucket: "finalproject-you.appspot.com",
+    messagingSenderId: "415521230405",
+    appId: "1:415521230405:web:e4f5034ea36ea7fd5e1d2c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyACpRha_w7SpbUU1_zmvwiaBMV16Cu7cMI",
+    authDomain: "finalproject-you.firebaseapp.com",
+    projectId: "finalproject-you",
+    storageBucket: "finalproject-you.appspot.com",
+    messagingSenderId: "415521230405",
+    appId: "1:415521230405:web:e4f5034ea36ea7fd5e1d2c"
+  }
 };
 
 /*
